d75dd7e25c56548e3a688fb9c2e6c489707854cc
JOELLE_STRESSLER <JES21N@FSU.EDU>
2024-10-27 16:16:11 -0400

    tic tac toe

--- sketch.js
+++ sketch.js
@@ -1,64 +1,57 @@
-// a simple game where you click to drop a ball and 
-// try to get as many balls as possible on the spinning shape
-// move the mouse up and down to change the rotation speed of the shape
-// use the right and left keys to make the shape larger or smaller
+let xImage, oImage;
+let cellSize = 130;
+let board;
+let currentPlayer = 'X';
 
-let spinningShape;
-let highScore = 0;
+function preload() {
+  xImage = loadImage("assets/x.png");
+  oImage = loadImage("assets/o.png");
+}
 
 function setup() {
-    let canvas = new Canvas("fullscreen");
-
-    world.gravity.y = 10;
-
-    // this sprite can be created on a single line, but it's easier to read this way:
-	spinningShape = new Sprite();
-	spinningShape.width = canvas.width/5;
-	spinningShape.height = spinningShape.width;
-    spinningShape.collider = "kinematic";
-
-    textFont("Courier", 24);
+  createCanvas(400, 400);
+  
+  // create the game board
+  board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
 }
 
 function draw() {
+  background(220);
+  
+  // Draw the grid
+  stroke(0);
+  strokeWeight(4);
+  line(cellSize, 0, cellSize, height);
+  line(cellSize * 2, 0, cellSize * 2, height);
+  line(0, cellSize, width, cellSize);
+  line(0, cellSize * 2, width, cellSize * 2);
 
-    // try the game without this line :)
-    clear();
+  // Draw X and O images based on board state
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      let x = col * cellSize + cellSize / 2;
+      let y = row * cellSize + cellSize / 2;
 
-    // the map function translates a value from one range to another
-    // https://p5js.org/reference/#/p5/map
-    spinningShape.rotationSpeed = map(mouse.y, 0, canvas.height, -10, 10);
-
-    // create a ball when the mouse is clicked
-    // https://p5play.org/learn/input_devices.html
-    if (mouse.presses()) {
-        let ball = new Sprite(mouse.x, -20, 20);
-        // make the ball resist rolling when it touches the spinning shape
-        // https://p5play.org/learn/sprite.html?page=9
-        ball.rotationDrag = 10;
+      if (board[row][col] === 'X') {
+        image(xImage, x - cellSize / 2, y - cellSize / 2, cellSize, cellSize);
+      } else if (board[row][col] === 'O') {
+        image(oImage, x - cellSize / 2, y - cellSize / 2, cellSize, cellSize);
+      }
     }
+  }
+}
 
-    // make the spinning shape larger or smaller using the keyboard
-    // note the difference between presses (above) and pressing (here)
-    if (kb.pressing('right')) {
-        spinningShape.width += 10;
-    } else if (kb.pressing('left')) {
-        spinningShape.width -= 10;
-    }
-
-    // loop through the allSprites array and see how many are above the center of the screen
-    let currentScore = 0;
-    for (let sprite of allSprites) {
-        if (sprite.y < canvas.height/2) {
-            currentScore++;
-            if (currentScore > highScore) {
-                highScore = currentScore;
-            }
-        }
-    }
-
-    // display the score (minus 1 so it doesn't count the spinning shape)
-    text("BALLS: " + (currentScore-1), 40, 60);
-    text("HIGH:  " + (highScore-1), 40, 86);
-
+function mousePressed() {
+  let row = floor(mouseY / cellSize);
+  let col = floor(mouseX / cellSize);
+  
+  // validating the clicks and updating board
+  if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] === '') {
+    board[row][col] = currentPlayer;
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  }
 }
